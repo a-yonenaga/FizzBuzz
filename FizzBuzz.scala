11767e8a8eeafdb7121d53a9bac2aea594f4dec6
Atsushi Yonenaga <a.yonenaga@gmail.com>
2020-05-30 00:43:16 +0900

    feature: 改行をやめてカンマに変更

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,13 +1,14 @@
 object Main extends App{
     for(i <- 1 to 100) {
         if ( i % 3 == 0 && i % 5 == 0) {
-            println("FizzBuzz")
+            print("FizzBuzz, ")
         } else if ( i % 3 == 0 ) {
-            println("Fizz")
+            print("Fizz, ")
         } else if ( i % 5 == 0 ) {
-            println("buzz")
+            print("buzz, ")
         } else {
-            println(i);
+            print(i + ", ")
         }
     }
 }
+
